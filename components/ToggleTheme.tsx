61166c4cde0bd64e03c237cd7a9b196b7969bf07
M.Ebad Arshad <m.ebadarshad2003@gmail.com>
2023-04-08 04:13:09 +0500

    started portfolio website which can be used by others as well

---
+++ components/ToggleTheme.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { FaMoon } from 'react-icons/fa'
+
+const ToggleTheme: FC = () => {
+    return (
+        <div className='fixed top-4 right-4 bg-white rounded-full text-black p-3 text-lg cursor-pointer'><FaMoon /></div>
+    )
+}
+
+export default ToggleTheme
